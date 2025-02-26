ff710707adf3540a13c8819480e0f79d0209f9d6
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2025-02-26 17:30:42 +0100

    build: eslint svelte rules

--- sveltekit-minimal-eslint-antfu/eslint.config.js
+++ sveltekit-minimal-eslint-antfu/eslint.config.js
@@ -1,5 +1,18 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
-  svelte: true,
-})
+export default antfu(
+  {
+    svelte: true,
+  },
+  {
+    rules: {
+      'svelte/html-quotes': [2, {
+        prefer: 'double', // or "single"
+        dynamic: {
+          quoted: false,
+          avoidInvalidUnquotedInHTML: false,
+        },
+      }],
+    },
+  },
+)
