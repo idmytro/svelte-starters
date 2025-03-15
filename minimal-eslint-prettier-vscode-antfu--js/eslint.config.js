b24f987343d8db7cad25d7d4d7e51aea9c89c31b
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2025-03-15 22:36:09 +0100

    feat: minimal-eslint-prettier-vscode-antfu--js

--- minimal-eslint-prettier-vscode-antfu--js/eslint.config.js
+++ minimal-eslint-prettier-vscode-antfu--js/eslint.config.js
@@ -1,34 +1,40 @@
-import prettier from 'eslint-config-prettier';
-import js from '@eslint/js';
-import { includeIgnoreFile } from '@eslint/compat';
-import svelte from 'eslint-plugin-svelte';
-import globals from 'globals';
-import { fileURLToPath } from 'node:url';
-import svelteConfig from './svelte.config.js';
-const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
+import { fileURLToPath } from 'node:url'
+import antfu from '@antfu/eslint-config'
+import { includeIgnoreFile } from '@eslint/compat'
+import js from '@eslint/js'
+import prettier from 'eslint-config-prettier'
+import svelte from 'eslint-plugin-svelte'
+import globals from 'globals'
+import svelteConfig from './svelte.config.js'
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-	includeIgnoreFile(gitignorePath),
-	js.configs.recommended,
-	...svelte.configs.recommended,
-	prettier,
-	...svelte.configs.prettier,
-	{
-		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node
-			}
-		}
-	},
-	{
-		files: ['**/*.svelte', '**/*.svelte.js'],
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url))
 
-		languageOptions: {
-			parserOptions: {
-				svelteConfig
-			}
-		}
-	}
-];
+export default antfu(
+  { svelte: true },
+  includeIgnoreFile(gitignorePath),
+  js.configs.recommended,
+  ...svelte.configs.recommended,
+  prettier,
+  ...svelte.configs.prettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    rules: {
+      'style/space-before-function-paren': [2, 'always'],
+    },
+  },
+  {
+    files: ['**/*.svelte', '**/*.svelte.js'],
+    languageOptions: {
+      parserOptions: {
+        svelteConfig,
+      },
+    },
+  },
+)
