11a450c688eddce61c6a38bdb3e22233f0e61c13
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2025-03-26 18:30:24 +0100

    fix: eslint import order

--- minimal-eslint-prettier-import/eslint.config.js
+++ minimal-eslint-prettier-import/eslint.config.js
@@ -1,15 +1,15 @@
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
+import perfectionist from 'eslint-plugin-perfectionist';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
 
 import svelteConfig from './svelte.config.js';
-const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
-import perfectionist from 'eslint-plugin-perfectionist';
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 export default ts.config(
 	includeIgnoreFile(gitignorePath),
