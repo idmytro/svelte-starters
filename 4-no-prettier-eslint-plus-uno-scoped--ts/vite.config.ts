78a9e4429f119229c737f83ae549577ef0aa9706
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2025-03-29 11:32:04 +0100

    4-no-prettier-eslint-plus-uno-scoped--ts

--- 4-no-prettier-eslint-plus-uno-scoped--ts/vite.config.ts
+++ 4-no-prettier-eslint-plus-uno-scoped--ts/vite.config.ts
@@ -1,6 +1,10 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite'
+import UnoCSS from '@unocss/svelte-scoped/vite'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+  plugins: [
+    sveltekit(),
+    UnoCSS(),
+  ],
+})
